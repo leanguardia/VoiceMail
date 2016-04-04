329ab408957e4a249ab2882b08dfb7eeb5411bf9
D. Leandro Guardia <leandro.guardia@percella.com>
2016-04-04 01:24:38 -0400

    layout made, and source code pasted

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,15 +4,20 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Voice Mail | Leandro Guardia");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        //primaryStage.initStyle(StageStyle.TRANSPARENT);
+        primaryStage.setTitle("VoiceMail | Leandro Guardia");
+
+        Scene myScene  = new Scene (root, 330, 500);
+        primaryStage.setScene(myScene);
         primaryStage.show();
     }
 
